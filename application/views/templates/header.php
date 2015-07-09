d011a12b59bebf23df448bb72a3a89bd839b0972
Greg Martin <gremartin@yahoo.com>
2015-07-08 21:23:08 -0400

    Fixed validation errors

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,20 +3,20 @@
 <head>
 	<title>Greg Martin - <?php echo $title ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" href="../../../css/gregmartinboston_master.css" type="text/css" media="screen, projection"/>
+	<link rel="stylesheet" href="../../../css/gregmartinboston_master.css" type="text/css" media="screen"/>
 	<!--<link rel="stylesheet" href="../../../css/gregmartinboston_master_alt.css" type="text/css" media="screen, projection"/>-->
 	<link rel="shortcut icon" href="../../../images/favicon.ico" type="image/x-icon"/>
 </head>
 <div id = "container">
 	<div id = "pagetop">
 		<div id= "logo_1">
-			   <img src="../../../images/guitar_logo.png" width="50" height="100">
+			   <img src="../../../images/guitar_logo.png" width="50" height="100" alt="guitar logo">
 		</div> <!-- end # logo_1 -->
 		<div id = "header">
 			<h1><a href="/">Greg Martin</a></h1>
 		</div>  <!-- end # header -->
 		<div id = "logo_2">
-			<img src="../../../images/bass_logo.png" width="135" height="75">
+			<img src="../../../images/bass_logo.png" width="135" height="75" alt="bass logo" >
 				</div> <!-- end #logo_2 -->
 	</div>  <!-- end # pagetop -->	
     <nav id = "nav_primary">
